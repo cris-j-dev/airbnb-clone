443ab34b62da9100eea5b2858a08cb62e34b9864
Chanhee <chjeong530@gmail.com>
2021-01-10 21:27:16 +0900

    #6.2 Custom Admin Functions

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,7 +13,67 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    pass
+    """ Room Admin Definition """
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        (
+            "Lsat Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_display = (
+        "name",
+        "description",
+        "country",
+        "city",
+        "price",
+        "address",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        print(obj)
+        return "Potato"
+
+    count_amenities.short_description = "test_amenities"
 
 
 @admin.register(models.Photo)
